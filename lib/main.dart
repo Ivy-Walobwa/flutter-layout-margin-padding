750abbd34a1b89e44ab42734800a40551e934a7a
Ivy-Walobwa <ivywalobwa@gmail.com>
2022-05-30 13:49:27 +0300

    container widget

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                myContainer("Text 1", width, height),
+                myContainer("Text 2", width, height)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                myContainer("Text 3", width, height),
+                myContainer("Text 4", width, height)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                myContainer("Text 5", width, height),
+                myContainer("Text 6", width, height)
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  Widget myContainer(String text, double width, double height) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+          horizontal: width * 0.1, vertical: height * 0.05),
+      padding: EdgeInsets.symmetric(
+          horizontal: width * 0.05, vertical: height * 0.05),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 32),
+      ),
+    );
+  }
+}
